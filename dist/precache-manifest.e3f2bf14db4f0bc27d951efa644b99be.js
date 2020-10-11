2bab790cd51d4232730d50d9cbb7611e1af99a87
shroomlife <robin@shroomlife.de>
2020-10-11 02:31:57 +0200

    added v2.0.0 deployment

---
+++ dist/precache-manifest.e3f2bf14db4f0bc27d951efa644b99be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95551a3f7f5656553d09",
+    "url": "/vegan.to/css/app.79027b6b.css"
+  },
+  {
+    "revision": "3acceda7b662189b8a597fa33a2a99fa",
+    "url": "/vegan.to/index.html"
+  },
+  {
+    "revision": "95551a3f7f5656553d09",
+    "url": "/vegan.to/js/app.363f15cc.js"
+  },
+  {
+    "revision": "6f24d1332f821d615f6f",
+    "url": "/vegan.to/js/chunk-vendors.53277b2a.js"
+  },
+  {
+    "revision": "2691aeff05924951c979e26b31821d0b",
+    "url": "/vegan.to/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vegan.to/robots.txt"
+  }
+]);
