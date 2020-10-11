0d99889def0dde232e89275cb60baa29614d8d94
shroomlife <robin@shroomlife.de>
2020-10-11 19:54:53 +0200

    added v2.1.0

---
+++ docs/precache-manifest.de7e379fe5f411bbcfcab75af2694df3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8202f35aa0713cb580567fa7338529",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "c6e18ef69c9e947a59ef",
+    "url": "/css/app.d6e7b72f.css"
+  },
+  {
+    "revision": "2943b77b7af4b7c2d7bf",
+    "url": "/css/chunk-vendors.5d6ed3db.css"
+  },
+  {
+    "revision": "54ffffe8ae7601e4c3966e1ce32c38b3",
+    "url": "/img/icon.png"
+  },
+  {
+    "revision": "81697e14d5c3d8c8bf80151b36041a93",
+    "url": "/img/icon.png~"
+  },
+  {
+    "revision": "21c4359703753eb162130bf819919f91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6e18ef69c9e947a59ef",
+    "url": "/js/app.f118851d.js"
+  },
+  {
+    "revision": "2943b77b7af4b7c2d7bf",
+    "url": "/js/chunk-vendors.7c2ef605.js"
+  },
+  {
+    "revision": "cfeb953dd542f72a31a8001a6b68f5cd",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
